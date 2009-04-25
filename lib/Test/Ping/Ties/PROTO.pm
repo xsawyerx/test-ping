987392f8b6aa357ed081002775586d93245ac26d
sawyer <sawyer@onion.(none)>
2009-04-26 00:15:43 +0300

    correct STORE

--- lib/Test/Ping/Ties/PROTO.pm
+++ lib/Test/Ping/Ties/PROTO.pm
@@ -2,10 +2,15 @@ package Test::Ping::Ties::PROTO;
 
 use strict;
 use warnings;
+
+use Net::Ping;
 use Tie::Scalar;
 
 sub TIESCALAR { return bless {}, shift;     }
-sub STORE     { $Test::Ping::PROTO = $_[1]; }
 sub FETCH     { return $Test::Ping::PROTO;  }
+
+sub STORE {
+    Test::Ping->_get_object() = Net::Ping->new($_[1]);
+}
 
 1;
